9de8dcdba33449ac8d7ce5ae78b324f715371491
sevavietl <sevavietl@gmail.com>
2017-08-30 22:36:09 +0300

    Fixed title for heroku.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'TODO') }}</title>
+    <title>TODO</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        TODO
                     </a>
                 </div>
 
