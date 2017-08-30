fae3810207e733f909f8e28f0e2ffd9ec1a03777
sevavietl <sevavietl@gmail.com>
2017-08-30 22:03:53 +0300

    Added heroku database configs.

--- config/database.php
+++ config/database.php
@@ -54,6 +54,21 @@ return [
             'engine' => null,
         ],
 
+        'heroku' => [
+            'driver' => 'mysql',
+            'host' => isset(parse_url(getenv("DATABASE_URL"))["host"]) ? parse_url(getenv("DATABASE_URL"))["host"] : '',
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'task_manager'),
+            'username' => isset(parse_url(getenv("DATABASE_URL"))["user"]) ? parse_url(getenv("DATABASE_URL"))["user"] : '',
+            'password' => isset(parse_url(getenv("DATABASE_URL"))["pass"]) ? parse_url(getenv("DATABASE_URL"))["pass"] : '',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
